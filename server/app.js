c3a81a24dde8839ccacfeb12eccf9832d7050430
rajeman <rojo.mosco@gmail.com>
2018-10-13 20:54:03 +0100

    ch(Setup server)

---
+++ server/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const port = process.env.PORT || 5008;
+const app = express();
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+  res.send({ message: 'Welcome to Store Manager' });
+});
+app.get('*', (req, res) => {
+  res.status(404).send({ error: 'Invalid Route' });
+}); 
+app.listen(port, () => {
+  console.log(`Listening on port ${port}.`);
+});
+
