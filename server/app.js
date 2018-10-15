ace925138afe5a08897f3a9c2d286f1970aeb9db
rajeman <rojo.mosco@gmail.com>
2018-10-15 11:42:04 +0100

    ft(Add sales route to app.js)

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import productsRouter from './routes/products';
+import salesRouter from './routes/sales';
 
 const port = process.env.PORT || 5008;
 const app = express();
 app.use(bodyParser.json());
 app.use('/api/v1/products', productsRouter);
+app.use('/api/v1/sales', salesRouter);
 app.get('/', (req, res) => {
   res.send({ message: 'Welcome to Store Manager' });
 });
