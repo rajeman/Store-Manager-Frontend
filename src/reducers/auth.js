d5fea4cf494ba9e0f657876357eb99e7a61d779c
Emmanuel Raji <emmanuel.raji@andela.com>
2019-02-03 16:46:19 +0100

    [#chore-separate-react]: refactor code

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -8,24 +8,26 @@ const defaultState = {
 
 export default (state = defaultState, action) => {
     switch (action.type) {
-        case 'SET_USER':
-            const userDetails = action.userDetails !== undefined ? action.userDetails : state.userDetails;
+        case 'SET_USER': {
+            const userDetails = action.userDetails || state.userDetails;
             return {
                 ...state,
                 userDetails
             }
-        case 'SET_LOGIN_STATE':
+        }
+        case 'SET_LOGIN_STATE': {
             const { loginState } = action;
             return {
                 ...state,
                 loginState
             }
-        case 'SET_LOGIN_ERROR':
+        }
+        case 'SET_LOGIN_ERROR': {
             const { loginError } = action;
             return {
                 ...state,
                 loginError
-            }
+            } }
         default:
             return state
     }
