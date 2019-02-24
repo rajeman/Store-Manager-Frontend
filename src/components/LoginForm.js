a364db720775f516652d227697c98804e6afb836
Emmanuel Raji <rojo.mosco@gmail.com>
2019-02-24 20:08:10 +0100

    [#164192525]: update dependencies

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,52 +4,49 @@ import { login } from '../actions/auth';
 
 class LoginForm extends React.Component {
 
-    render() {
-        const { loginState, loginError} = this.props.auth;
-        const onFormSubmit = (e) => {
-           //console.log(this.props);
-            e.preventDefault();
-            const email = e.target.elements.uname.value.trim();
-            const password = e.target.elements.psw.value.trim();
-            this.props.dispatch(login(email, password));
-            
-        };
-        return (
-            
-            <div className="cont">
-                <section>
-                    <div id="user-form">
-                        <div className="container">
-                            <form onSubmit={onFormSubmit} >
-                                <h3>User Login</h3>
-                                <div>
-                                    <label>Email:</label>
-                                    <br />
-                                    <input type="email" id="email" placeholder="Enter Email" name="uname" required></input>
-                                </div>
-                                <div>
-                                    <label>Password:</label>
-                                    <br />
-                                    <input type="password" id="password" placeholder="Enter Password" name="psw" required></input>
-                                </div>
-                                <button type="submit">Login</button>
-                                <br />
-                                <div className="not-registered">
-                                    {loginState === 'STATE_LOGIN_FAILED' && <span className="pop-up">{loginError}</span>}
-                                    {loginState === 'STATE_LOGGING_IN' && <span className="lds-hourglass"></span>}
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </section>
+  render() {
+    const { loginState, loginError } = this.props.auth;
+    const onFormSubmit = (e) => {
+      e.preventDefault();
+      const email = e.target.elements.uname.value.trim();
+      const password = e.target.elements.psw.value.trim();
+      this.props.dispatch(login(email, password));
+    };
+    return (
+      <div className="cont">
+        <section>
+          <div id="user-form">
+            <div className="container">
+              <form onSubmit={onFormSubmit} >
+                <h3>User Login</h3>
+                <div>
+                  <label>Email:</label>
+                  <br />
+                  <input type="email" id="email" placeholder="Enter Email" name="uname" required></input>
+                </div>
+                <div>
+                  <label>Password:</label>
+                  <br />
+                  <input type="password" id="password" placeholder="Enter Password" name="psw" required></input>
+                </div>
+                <button type="submit">Login</button>
+                <br />
+                <div className="not-registered">
+                  {loginState === 'STATE_LOGIN_FAILED' && <span className="pop-up">{loginError}</span>}
+                  {loginState === 'STATE_LOGGING_IN' && <span className="lds-hourglass"></span>}
+                </div>
+              </form>
             </div>
-        );
+          </div>
+        </section>
+      </div>
+    );
 
-    }
+  }
 }
 
 const mapStateToProps = (state) => state;
 
-export default connect(mapStateToProps)(LoginForm); 
+export default connect(mapStateToProps)(LoginForm);
 
 
