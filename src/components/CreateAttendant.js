bfd775cc6d3cab5b2f76dec1807dbb17d7d9c91c
Emmanuel Raji <rojo.mosco@gmail.com>
2019-03-05 08:15:02 +0100

    [#164192525]: add test for attendnat reducers

--- src/components/CreateAttendant.js
+++ src/components/CreateAttendant.js
@@ -6,30 +6,29 @@ import Footer from './Footer';
 import AttendantCreateForm from './AttendantCreateForm';
 import NotFoundPage from './NotFoundPage';
 
-
 class CreateAttendant extends React.Component {
-    componentDidMount(){
-        this.props.dispatch(fetchUser());
+  componentDidMount() {
+    this.props.dispatch(fetchUser());
+  }
+  render() {
+    const { level } = this.props.auth.userDetails;
+    if (level === 1) {
+      return (
+        <NotFoundPage />
+      )
     }
-    render() {
-        const { level } = this.props.auth.userDetails;
-        if(level === 1){
-            return (
-                <NotFoundPage />
-            )
-        }
-        return level === 2 ? (
-            <div id = "cover">
-                <Header />
-                <AttendantCreateForm />
-                <Footer />
-            </div>
-        ) : <div></div>
+    return level === 2 ? (
+      <div id="cover">
+        <Header />
+        <AttendantCreateForm />
+        <Footer />
+      </div>
+    ) : <div></div>
 
-    }
+  }
 }
 
 const mapStateToProps = (state) => state;
 
-export default connect(mapStateToProps)(CreateAttendant); 
+export default connect(mapStateToProps)(CreateAttendant);
 
