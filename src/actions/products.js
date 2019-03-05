27a6056384b6358f7b300a32ce1d0f6b67e29c06
Emmanuel Raji <rojo.mosco@gmail.com>
2019-03-05 08:13:49 +0100

    [#164192525]: add test for reducers

--- src/actions/products.js
+++ src/actions/products.js
@@ -5,11 +5,11 @@ import paths from '../helpers/paths';
 
 const url = 'https://onlinestoremanager.herokuapp.com/api/v1/products';
 
-export const setProductLoading = () => (
-  {
-    type: 'SET_PRODUCT_LOADING',
-  }
-);
+// export const setProductLoading = () => (
+//   {
+//     type: 'SET_PRODUCT_LOADING',
+//   }
+// );
 
 export const setProductError = error => (
   {
--- src/components/AttendantCreateForm.js
+++ src/components/AttendantCreateForm.js
@@ -4,46 +4,45 @@ import { createAttendant } from '../actions/attendants'
 
 class AttendantCreateForm extends React.Component {
 
-    render() {
-        const onFormSubmit = (e) => {
-            //console.log(this.props);
-             e.preventDefault();
-             const name = e.target.elements.aname.value.trim();
-             const email = e.target.elements.aemail.value.trim();
-             this.props.dispatch(createAttendant(name, email));
-         }
-        const { attendantCreateState, attendantCreateError} = this.props.attendants;
-        return (
-            <div className="cont">
-                <section>
-                    <div id="user-form">
-                        <div className="container">
-                            <form onSubmit = {onFormSubmit}>
-                                <h3>New Attendant</h3>
-                                <div>
-                                    <label>Name:</label>
-                                    <br />
-                                    <input type="text" id="name" placeholder="Enter Attendant's Name" name="aname" pattern=".{3,}" required title="Name must be at least 3 characters"></input>
-                                </div>
-                                <div>
-                                    <label>Email:</label>
-                                    <br />
-                                    <input type="email" id="email" placeholder="Enter Attendant's Email" name="aemail" required></input>
-                                </div>
-                                <div className="not-registered">
-                                    {attendantCreateState === 'STATE_CREATE_FAILED' && <span className="pop-up">{attendantCreateError}</span>}
-                                    {attendantCreateState === 'STATE_CREATING' && <span className="lds-hourglass"></span>}
-                                </div>
-                                <button type="submit">Create</button>
-                                <br />
-                            </form>
-                        </div>
-                    </div>
-                </section>
-            </div>
-        );
-
+  render() {
+    const onFormSubmit = (e) => {
+      e.preventDefault();
+      const name = e.target.elements.aname.value.trim();
+      const email = e.target.elements.aemail.value.trim();
+      this.props.dispatch(createAttendant(name, email));
     }
+    const { attendantCreateState, attendantCreateError } = this.props.attendants;
+    return (
+      <div className="cont">
+        <section>
+          <div id="user-form">
+            <div className="container">
+              <form onSubmit={onFormSubmit}>
+                <h3>New Attendant</h3>
+                <div>
+                  <label>Name:</label>
+                  <br />
+                  <input type="text" id="name" placeholder="Enter Attendant's Name" name="aname" pattern=".{3,}" required title="Name must be at least 3 characters"></input>
+                </div>
+                <div>
+                  <label>Email:</label>
+                  <br />
+                  <input type="email" id="email" placeholder="Enter Attendant's Email" name="aemail" required></input>
+                </div>
+                <div className="not-registered">
+                  {attendantCreateState === 'STATE_CREATE_FAILED' && <span className="pop-up">{attendantCreateError}</span>}
+                  {attendantCreateState === 'STATE_CREATING' && <span className="lds-hourglass"></span>}
+                </div>
+                <button type="submit">Create</button>
+                <br />
+              </form>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+
+  }
 }
 
 const mapStateToProps = (state) => state;
